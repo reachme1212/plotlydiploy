fca0ff6c7bb6670e759ca8055ba19d17ae981aa3
Sangee_uofa <reachme1212@gmail.com>
2021-12-03 15:17:16 -0700

    Mod 12 challenge finished

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # plotlydiploy
+
+**Overview of the analysis:**
+
+In this challenge I am helping a microbiologist named 'Rosa', Rosa's hypothesis is human belly button carry particular type of bacteria that can synthesize protein in a way that taste like beef(not to worry microbes in human navel are harmless), Rosa is trying to identify that single bacterium among the large sample, so they can collaborate with a manufacturer to create synthetic beef from human body bacteria's . 
+
+In the past scientists took cells from a cow and, at an institute in the Netherlands, turned them into strips of muscle that they combined to make a patty. Prof Mark Post, of Maastricht University, the scientist behind the burger, remarked: "It's a very good start."
+
+
+**Dashboard Details:**
+ 
+To help Rosa I have created an interactive chart that can filter and show data for each individual, I have received data in Json format, I have written JavaScript code to read and convert the data to the charts in dashboard. I received 3 sets of data in that json file metadata {}, names {} and samples {}.
+
+I have created a bar chart to show top 10 species of bacteria for a particular individual, Test Subject Id no represents the person donated microbes’ samples and details related to microbes in their belly button is stored in sample {}, So Rosa can filter each person and check the diversity of the microbes and pick the one she is looking for to create the synthetic beef.Each persons  individual profiles are bacterially unique. Washing frequency can be one important detail that affects what type of bacteria a person carries so to see correlation I have created a guage chart for washing frequency as well.
+
+![image](key_value.PNG)
+
+![image](bar.PNG)
+
+![image](bubble.PNG)
+
+![image](gauge.PNG)
+
+**Dashboard** [lets go to](https://reachme1212.github.io/plotlydiploy/)
+
---
+++ charts.js
@@ -0,0 +1,152 @@
+function init() {
+
+    var selector = d3.select("#selDataset");
+    d3.json("samples.json").then((data) => {
+        console.log(data);
+        var sampleNames = data.names;
+        sampleNames.forEach((sample) => {
+            selector
+                .append("option")
+                .text(sample)
+                .property("value", sample);
+        });
+
+        // Use the first sample from the list to build the initial plots
+        var firstSample = sampleNames[0];
+        buildCharts(firstSample);
+        buildMetadata(firstSample);
+    });
+}
+init();
+
+function optionChanged(newSample) {
+
+    // Fetch new data each time a new sample is selected
+    buildMetadata(newSample);
+    buildCharts(newSample);
+
+}
+
+//Demographics Panel
+
+function buildMetadata(sample) {
+
+    d3.json("samples.json").then((data) => {
+        console.log(data);
+        var metadata = data.metadata;
+        var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+        var result = resultArray[0];
+        var PANEL = d3.select("#sample-metadata");
+        PANEL.html("");
+        Object.entries(result).forEach(([key, value]) => {
+            PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+        });
+
+    });
+}
+
+//  Create the buildCharts function.
+function buildCharts(sample) {
+
+    // Use `d3.json` to fetch the sample data for the plots
+    d3.json("samples.json").then((data) => {
+        console.log(data);
+
+        // hold data from sample {}
+        var samples = data.samples;
+        var resultsarray = samples.filter(sampleobject => sampleobject.id == sample);
+        var result = resultsarray[0];
+
+        //  hold data from metadata {}
+        var metadata = data.metadata;
+        var metaArray = metadata.filter(sampleobject => sampleobject.id == sample);
+        var metaResult = metaArray[0];
+
+        var ids = result.otu_ids;
+        var labels = result.otu_labels;
+        var values = result.sample_values;
+
+        var wref = parseFloat(metaResult.wfreq);
+
+        //console.log(wref);
+
+        var yticks = ids.map(id => `OTU ${id}`).slice(0, 10).reverse();
+
+        var barData = {
+            x: values.slice(0, 10).reverse(),
+            y: yticks,
+            text: labels.slice(0, 10).reverse(),
+            type: "bar",
+            orientation: "h"
+        };
+
+        var traceBar = [barData];
+
+        var barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: {
+                l: 100,
+                r: 100,
+                t: 100,
+                b: 100
+            },
+            xaxis: { title: "Sample Values" },
+            yaxis: { title: "Bacteria ID" }
+        };
+
+        Plotly.newPlot("bar", traceBar, barLayout);
+
+        var bubbleData = [{
+            x: ids,
+            y: values,
+            text: labels,
+            mode: "markers",
+            marker: {
+                color: ids,
+                size: values,
+                colorscale: "Earth"
+            }
+        }];
+
+        var bubbleLayout = {
+            title: "Bacteria Cultures Per Sample",
+            xaxis: { title: "OTU ID" },
+            hovermode: "closest",
+        };
+
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+        var gaugeData = [{
+            domain: { x: [0, 1], y: [0, 1] },
+            value: wref,
+            title: { text: "Belly Button Washing Frequency" },
+            type: "indicator",
+            mode: "gauge+number",
+            gauge: {
+                axis: {
+                    range: [null, 10]
+
+                },
+                bar: { color: "#424949 " },
+                steps: [{ range: [0, 2], color: "#EC7063" },
+                    { range: [2, 4], color: "#F5B041" },
+                    { range: [4, 6], color: "#F7DC6F " },
+                    { range: [6, 8], color: "#82E0AA" },
+                    { range: [8, 10], color: "#1E8449 " },
+                ]
+
+            },
+
+
+            marker: {
+
+            },
+
+        }];
+
+        var gaugeLayout = { width: 500, height: 500, margin: { t: 0, b: 0 } };
+
+        Plotly.newPlot('gauge', gaugeData, gaugeLayout);
+
+    });
+}
